769277f66e5b85564c1260d00d1e86c237ea2b23
Miran Zagragja <miran97@live.no>
2019-06-29 16:29:31 +0200

    Updates

--- FilEdit.java
+++ FilEdit.java
@@ -13,6 +13,11 @@ public class FilEdit{
             String linje = sc.nextLine();
              while(sc.hasNextLine()){
                 if(linje.startsWith("1")){
+                    linje = linje.replaceAll("Gf. 4", "Gf4");
+                    linje = linje.replaceAll("Gf.2", "Gf2");
+                    linje = linje.replaceAll("Ht.1", "Ht1");
+                    linje = linje.replaceAll("Gf.1", "Gf1");
+                    linje = linje.replaceAll("	", " ");
                     skriv.write(linje + "\n");
                 }
                 linje = sc.nextLine();
